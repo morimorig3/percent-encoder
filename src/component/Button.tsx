0985f73f62eaef9ed30cf6d2e3fbc4af16685b73
morimorig3 <mori.nto@outlook.jp>
2021-12-15 09:50:17 +0900

    ボタンコンポーネントの状態をコンポーネント内部で切り分けるようisClickable属性を追加

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -4,12 +4,15 @@ type Props = {
   onClick: () => void;
   children: ReactNode;
   className?: string;
+  isClickable?: boolean;
 };
 
-const Button: VFC<Props> = ({ children = 'OK', onClick = () => undefined, className = '' }) => (
+const Button: VFC<Props> = ({ children = 'OK', onClick = () => undefined, className = '', isClickable = false }) => (
   <button
-    className={`border-2 transition-colors font-bold py-2 px-4 rounded ${className}`}
-    onClick={onClick}
+    className={`border-2 transition-colors font-bold py-2 px-4 rounded ${className} ${
+      isClickable ? '' : 'pointer-events-none opacity-50'
+    }`}
+    onClick={isClickable ? onClick : (e) => e.preventDefault()}
     type="button"
   >
     {children}
